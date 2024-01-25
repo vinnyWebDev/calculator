c175bf582f2d01abc9736ba3fd3244346fc581a9
Vinny Murphy <vinnywebdev@gmail.com>
2024-01-25 12:19:17 +0000

    updated dom manipulation

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 let num1;
 let num2;
 let operator;
+let screenValue;
 
-const calcBtn = document.querySelector("#calcBtn");
-calcBtn.addEventListener("click", () => {
-  console.log("test");
+const numBtns = document.querySelectorAll(".calcBtn");
+const screen = document.querySelector("#calcScreen");
+
+//Create the functions that populate the display when you click the number buttons.
+numBtns.forEach((numBtn) => {
+  numBtn.addEventListener("click", () => {
+    //for display purposes, we're appending the input value to the screen node
+    const num = document.createElement("text");
+    num.textContent = numBtn.value;
+    screen.appendChild(num);
+    screenValue = screen.textContent;
+    console.log(screenValue);
+  });
 });
 
 //add
@@ -45,4 +56,13 @@ const operate = (operator, num1, num2) => {
   }
 };
 
-console.log(operate("+", 3, 9));
+/*
+Make the calculator work! You’ll need to store the first number and second number that are input into the calculator, 
+utilize the operator that the user selects, and then operate() on the two numbers when the user presses the “=” key.
+
+You should already have the code that can populate the display, so once operate() has been called, update the display 
+with the ‘solution’ to the operation. This is the hardest part of the project. 
+
+You need to figure out how to store all the values and call the operate function with them. 
+Don’t feel bad if it takes you a while to figure out the logic.
+*/
