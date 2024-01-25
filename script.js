769d9dbb1d98c1f5a27f97f41c6c73463694f9d9
Vinny Murphy <vinnywebdev@gmail.com>
2024-01-25 10:47:36 +0000

    added functions and basic front end

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let num1;
 let num2;
 let operator;
 
+const calcBtn = document.querySelector("#calcBtn");
+calcBtn.addEventListener("click", () => {
+  console.log("test");
+});
+
 //add
 const add = function (a, b) {
   return a + b;
@@ -21,3 +26,23 @@ const multiply = function (arr) {
 const divide = function (a, b) {
   return a / b;
 };
+
+//will call a different opperation depending on opperator
+const operate = (operator, num1, num2) => {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+      break;
+    case "-":
+      return subtract(num1, num2);
+      break;
+    case "*":
+      return multiply(num1, num2);
+      break;
+    case "/":
+      return divide(num1, num2);
+      break;
+  }
+};
+
+console.log(operate("+", 3, 9));
